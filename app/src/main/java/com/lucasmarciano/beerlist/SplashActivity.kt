d1df6dcc6e833860fc28f8661e591488165481ef
Lucas Marciano <lucas.almeida.marciano@gmail.com>
2018-07-20 17:29:53 -0300

    Second commit

---
+++ app/src/main/java/com/lucasmarciano/beerlist/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.lucasmarciano.beerlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
